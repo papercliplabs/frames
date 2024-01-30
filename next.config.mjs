8444c0560d58cd62cc0d2230e00ad8f17146524a
spennyp <hello@spencerperkins.me>
2024-01-30 15:29:34 -0800

    Fix builder redirects

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,16 @@
 const nextConfig = {
     redirects: async () => {
         return [
-          {
-            source: "/redirects/:route",
-            destination: "https://:route",
-            permanent: false,
-          },
+            {
+                source: "/redirects/nouns.build/dao/:chain/:token",
+                destination: "https://nouns.build/dao/:chain/:token",
+                permanent: false,
+            },
+            {
+               source: "/redirects/:route*",
+               destination: "https://:route*",
+               permanent: false,
+            },
         ];
       },
 };
