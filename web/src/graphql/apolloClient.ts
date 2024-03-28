dc8d98adbef419d64adfaaa309b58e427dafb5c3
spennyp <hello@spencerperkins.me>
2024-03-27 18:58:43 -0700

    Added superrare live uaction frame

---
+++ web/src/graphql/apolloClient.ts
@@ -0,0 +1,11 @@
+import { HttpLink, InMemoryCache, ApolloClient } from "@apollo/client";
+import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+
+export const { getClient: getSuperrareApolloClient } = registerApolloClient(() => {
+    return new ApolloClient({
+        cache: new InMemoryCache(),
+        link: new HttpLink({
+            uri: "https://api.rare.xyz/v1/graphql",
+        }),
+    });
+});
