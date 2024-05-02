ab0c2751cde6424700d0316636b58c49b76c08a4
spennyp <hello@spencerperkins.me>
2024-05-02 14:58:41 -0700

    relax cors for superrare

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    headers: async () => {
+      return [
+        {
+          source: "/superrare/:path*",
+          headers: [
+            {
+              key: "Access-Control-Allow-Origin",
+              value: "*", // Set your origin
+            },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET, POST, PUT, DELETE, OPTIONS",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value: "Content-Type, Authorization",
+            },
+          ],
+        },
+      ];
+    },
     redirects: async () => {
         return [
             {
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,16 @@
+import { getFrameMetadata } from "frog/next";
+import type { Metadata } from "next";
+import { useEffect } from "react";
+
+// export async function generateMetadata(): Promise<Metadata> {
+//   const frameMetadata = await getFrameMetadata(
+//     "https://frames.paperclip.xyz/superrare/router/0xca53bb6cdfcd5bf437bf4ac6d17c3b0e67d8a83e/18"
+//   );
+//   return {
+//     other: frameMetadata,
+//   };
+// }
+
 export default function Page() {
   return <>HOME</>;
 }
