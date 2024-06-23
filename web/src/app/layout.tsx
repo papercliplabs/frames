03b5cacf416ec12f87a8cc37c1d40bff91bf8085
spennyp <hello@spencerperkins.me>
2024-06-23 13:10:12 -0700

    added google analytics

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,12 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/react";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
+      <GoogleAnalytics gaId="G-CQ9F69L3XX" />
     </html>
   );
 }
